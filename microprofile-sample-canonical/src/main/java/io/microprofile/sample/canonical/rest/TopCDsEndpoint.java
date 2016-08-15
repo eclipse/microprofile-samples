1ca2f5d9e9e2df4ee7d9d4eebe8e51bb8c48ba99
AndyGee <andy.is@gmx.de>
2016-08-15 21:45:05 +0200

    Apache 2.0 License

--- microprofile-sample-canonical/src/main/java/io/microprofile/sample/canonical/rest/TopCDsEndpoint.java
+++ microprofile-sample-canonical/src/main/java/io/microprofile/sample/canonical/rest/TopCDsEndpoint.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.microprofile.sample.canonical.rest;
 
 import io.microprofile.sample.canonical.utils.QLogger;
@@ -25,17 +41,17 @@ public class TopCDsEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public String getTopCDs() {
 
-        JsonArrayBuilder array = Json.createArrayBuilder();
-        List<Integer> randomCDs = getRandomNumbers();
-        for (Integer randomCD : randomCDs) {
+        final JsonArrayBuilder array = Json.createArrayBuilder();
+        final List<Integer> randomCDs = getRandomNumbers();
+        for (final Integer randomCD : randomCDs) {
             array.add(Json.createObjectBuilder().add("id", randomCD));
         }
         return array.build().toString();
     }
 
     private List<Integer> getRandomNumbers() {
-        List<Integer> randomCDs = new ArrayList<>();
-        Random r = new Random();
+        final List<Integer> randomCDs = new ArrayList<>();
+        final Random r = new Random();
         randomCDs.add(r.nextInt(100) + 1101);
         randomCDs.add(r.nextInt(100) + 1101);
         randomCDs.add(r.nextInt(100) + 1101);
--- microprofile-sample-swagger/src/main/java/io/microprofile/sample/swagger/rest/RestApplication.java
+++ microprofile-sample-swagger/src/main/java/io/microprofile/sample/swagger/rest/RestApplication.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.microprofile.sample.swagger.rest;
 
 import io.swagger.jaxrs.config.BeanConfig;
@@ -13,7 +29,7 @@ import java.util.Set;
 public class RestApplication extends Application {
 
     public RestApplication() {
-        BeanConfig beanConfig = new BeanConfig();
+        final BeanConfig beanConfig = new BeanConfig();
         beanConfig.setTitle("TOP CDs");
         beanConfig.setDescription("Gives the TOP selling CDs");
         beanConfig.setVersion("1.0.0");
@@ -27,7 +43,7 @@ public class RestApplication extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new HashSet();
+        final Set<Class<?>> resources = new HashSet();
 
         resources.add(TopCDsEndpoint.class);
         resources.add(ApiListingResource.class);
