92869715abafda4647b93ad981f23a815321c0f3
John D. Ament <johndament@apache.org>
2016-09-25 22:10:01 -0400

    Updated license headers to match format.  Removed duplicate license headers in files.

--- microprofile-sample-swagger/src/main/java/io/microprofile/sample/swagger/rest/SwaggerApiListing.java
+++ microprofile-sample-swagger/src/main/java/io/microprofile/sample/swagger/rest/SwaggerApiListing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hammock and its contributors
+ * Copyright (C) 2016 Microprofile.io and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
